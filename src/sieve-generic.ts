08a4e4d32077561a1634aae3e76b721f830f9db1
Vitaly Tomilov <vitaly.tomilov@gmail.com>
2021-09-28 04:32:59 +0100

    improving generic sieve

--- src/sieve-generic.ts
+++ src/sieve-generic.ts
@@ -1,17 +1,17 @@
 import {stopOnCount} from './stop';
 
 const sieveNumbers = {
-    start: [2, 3, 5],
+    start: [2, 3, 5, 7],
     a: 3,
-    b: 7,
+    b: 9,
     c: 2
 };
 
 const sieveBigInt = {
-    start: [2n, 3n, 5n],
-    a: 3n,
-    b: 7n,
-    c: 2n
+    start: sieveNumbers.start.map(BigInt),
+    a: BigInt(sieveNumbers.a),
+    b: BigInt(sieveNumbers.b),
+    c: BigInt(sieveNumbers.c)
 };
 
 export function sieve_Number(): IterableIterator<number> {
@@ -23,9 +23,9 @@ export function sieve_BigInt(): IterableIterator<bigint> {
 }
 
 function* sieveGeneric<T>(params: any): IterableIterator<T> {
-    yield params.start[0];
-    yield params.start[1];
-    yield params.start[2];
+    for (const a of params.start) {
+        yield a;
+    }
     const sieve = new Map<number, number>();
     const ps = sieveGeneric(params);
     ps.next() && ps.next();
@@ -48,4 +48,4 @@ function* sieveGeneric<T>(params: any): IterableIterator<T> {
     }
 }
 
-console.log('result:', [...stopOnCount(sieve_Number(), 10)]);
+console.log('result:', [...stopOnCount(sieve_BigInt(), 10)]);
