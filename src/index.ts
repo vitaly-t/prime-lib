fa42a4c16fdb32c5f2b09179639f948a147bded7
Vitaly Tomilov <vitaly.tomilov@gmail.com>
2021-10-02 20:42:34 +0100

    adding maxPrime

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,12 @@ import {sieveInt, sieveIntStart, sieveBigInt, sieveBigIntStart} from './sieve';
 export {stopWhen, stopOnCount, stopOnValue} from './stop';
 export {isPrime} from './is-prime';
 
+/**
+ * Maximum prime number that can be generated in JavaScript,
+ * using the standard 'number' type (53-bit of integer range).
+ */
+export const maxPrime = 9_007_199_254_740_881;
+
 export function generatePrimes(start?: number): IterableIterator<number> {
     return start && start > 2 ? sieveIntStart(start) : sieveInt();
 }
