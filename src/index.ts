1c0a327344f98f9bb1c26cbe4f1c4110537a24dd
Vitaly Tomilov <vitaly.tomilov@gmail.com>
2021-10-03 10:46:01 +0100

    updating package

--- src/index.ts
+++ src/index.ts
@@ -10,16 +10,16 @@ export {isPrime} from './is-prime';
 export const maxPrime = 9_007_199_254_740_881;
 
 /**
- * Infinite generator of prime numbers, based on Sieve of Eratosthenes algorithm.
- * The algorithm extended to support an optional start prime.
+ * Infinite generator of prime numbers, based on Sieve of Eratosthenes algorithm,
+ * extended to support optional start prime.
  */
 export function generatePrimes(start?: number): IterableIterator<number> {
     return start && start > 2 ? sieveIntStart(start) : sieveInt();
 }
 
 /**
- * Infinite generator of BigInt prime numbers, based on Sieve of Eratosthenes algorithm.
- * The algorithm extended to support an optional start prime.
+ * Infinite generator of BigInt prime numbers, based on Sieve of Eratosthenes algorithm,
+ * extended to support optional start prime.
  */
 export function generateBigPrimes(start?: bigint): IterableIterator<bigint> {
     return start && start > 2n ? sieveBigIntStart(start) : sieveBigInt();
