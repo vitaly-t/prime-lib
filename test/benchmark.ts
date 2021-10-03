dfa04e0b0f95923377e7fe44b566b286752bc8eb
Vitaly Tomilov <vitaly.tomilov@gmail.com>
2021-10-03 15:17:34 +0100

    adding benchmarks

--- test/benchmark.ts
+++ test/benchmark.ts
@@ -1,26 +1,53 @@
 // tslint:disable:no-console
 
+import {sieveInt} from '../dist/sieve';
+
 interface ITestResult {
-    [name: string]: number;
+    [name: string]: any;
 }
 
 function testSieveInt(): ITestResult {
-    console.log('Running test: testSieveInt...');
-    return {};
+    const tests = {
+        '1mln': {
+            max: 1_000_000,
+            desc: 'Time in milliseconds to generate the first million primes'
+        },
+        '10mln': {
+            max: 10_000_000,
+            desc: 'Time in milliseconds to generate the first 10 million primes'
+        },
+        '100mln': {
+            max: 100_000_000,
+            desc: 'Time in milliseconds to generate the first 100 million primes'
+        }
+    };
+    const result = {};
+    for (const t in tests) {
+        const i = sieveInt();
+        const maxPrimes = tests[t].max;
+        const start = Date.now();
+        let count = 0, p;
+        do {
+            p = i.next();
+        } while (++count < maxPrimes);
+        result[t] = {
+            'ms': Date.now() - start,
+            'Last Prime': p.value,
+            'Description': tests[t].desc
+        };
+    }
+    return result;
 }
 
 function testSieveIntStart(): ITestResult {
-    console.log('Running test: testSieveIntStart...');
     return {};
 }
 
 function testSieveBigInt(): ITestResult {
-    console.log('Running test: testSieveBigInt...');
     return {};
 }
 
 function testSieveBigIntStart(): ITestResult {
-    console.log('Running test: testSieveBigIntStart...');
     return {};
 }
 
@@ -51,21 +78,24 @@ function testSieveBigIntStart(): ITestResult {
     if (process.argv.length > 2) {
         let i = 2;
         do {
-            printResult(runTest(process.argv[i]));
+            runTest(process.argv[i]);
         } while (++i < process.argv.length);
     } else {
-        printResult(runTest('all'));
+        runTest('all');
     }
 
-    function runTest(testName: string): ITestResult | ITestResult[] {
+    function runTest(testName: string): void {
         if (testName in commands) {
-            return commands[testName]();
+            if (testName === 'all') {
+                console.log('*** Running all tests...');
+            } else {
+                console.log(`*** Running tests for ${JSON.stringify(testName)}...`);
+            }
+            const result = commands[testName]();
+            console.table(result);
+        } else {
+            console.error(`Unknown test ${JSON.stringify(testName)}\n`);
+            process.exit(0);
         }
-        console.error(`Unknown test ${JSON.stringify(testName)}\n`);
-        process.exit(0);
-    }
-
-    function printResult(result: ITestResult | ITestResult[]) {
-        console.table(result, ['duration (ms)']);
     }
 })();
