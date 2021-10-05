9f80b42ca7e8feb3419b34b6e073a7bcf81f5afd
Vitaly Tomilov <vitaly.tomilov@gmail.com>
2021-10-05 03:27:28 +0100

    adding extra prime test code

--- src/is-prime.ts
+++ src/is-prime.ts
@@ -71,3 +71,47 @@ function isBigPrime(n: bigint) {
     }
     return true;
 }
+
+// This one needs testing, but appears to be the fastest so far:
+// https://github.com/henrikfredriksson/prime-q/blob/master/index.js
+
+// NOTE: I tested it for 5, and it didn't work :(
+export function isPrime_MillerRabin(n: number, k = 100) {
+    if (n === 2 || n === 3) {
+        return true;
+    }
+    const smallPrimes = [2, 3, 5, 7, 11, 13, 17];
+    const res = smallPrimes.map(x => n % x === 0).filter(x => x).includes(true);
+    if (res) {
+        return false;
+    }
+    if (n % 2 === 0 || n % 3 === 0 || n % 5 === 0 || n % 7 === 0 || n < 2) {
+        return false;
+    }
+    let s = 0;
+    let d = n - 1;
+    while (d % 2 === 0) {
+        d /= 2;
+        ++s;
+    }
+    const witness = a => Math.floor(Math.random() * (a - 2)) + 2;
+
+    while (k--) {
+        let a = Math.pow(witness(n), s);
+        if (a % n === 1) {
+            continue;
+        }
+        let testFails = false;
+        for (let i = 0; i < s; i++) {
+            a = Math.pow(a, 2) % n;
+            if (a === n - 1) {
+                testFails = true;
+                break;
+            }
+        }
+        if (testFails) {
+            return false;
+        }
+    }
+    return true;
+}
