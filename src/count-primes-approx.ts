9d629ec12a6a87ef3fb18d730c9c90b6dd70d69c
Vitaly Tomilov <vitaly-t@users.noreply.github.com>
2021-10-25 11:57:44 +0100

    improve count-primes-approx performance

--- src/count-primes-approx.ts
+++ src/count-primes-approx.ts
@@ -1,20 +1,30 @@
 import {IPrimeApprox} from './common';
+import {sieveIntBoost} from './soe-generators';
 
 /**
- Pierre Dusart's formula works when x > 598,
- so we need pre-calculated first 108 primes, up to 593:
+ Pierre Dusart's formula works when x > 598
  */
-const primes = [
-    2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37,
-    41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89,
-    97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151,
-    157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223,
-    227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281,
-    283, 293, 307, 311, 313, 317, 331, 337, 347, 349, 353, 359,
-    367, 373, 379, 383, 389, 397, 401, 409, 419, 421, 431, 433,
-    439, 443, 449, 457, 461, 463, 467, 479, 487, 491, 499, 503,
-    509, 521, 523, 541, 547, 557, 563, 569, 571, 577, 587, 593
-];
+const calcLimit = 598;
+
+/**
+ * The last count is 108, so a single byte is sufficient.
+ */
+const countsCache = new Uint8Array(calcLimit - 1);
+
+/**
+ * For best performance, we cache up 597 first counts here.
+ */
+(function prepareCounts() {
+    const p = [...sieveIntBoost(108)];
+    let k = 0, count = 1;
+    for (let i = 2; i <= calcLimit; i++) {
+        if (i === p[k + 1]) {
+            k++;
+            count++;
+        }
+        countsCache[i - 2] = count;
+    }
+})();
 
 /**
  * Approximate primes count calculation, up to the value of x, based on Pierre Dusart's formula:
@@ -25,7 +35,7 @@ const primes = [
  * See: https://primes.utm.edu/howmany.html
  */
 export function countPrimesApprox(x: number): IPrimeApprox {
-    if (x > 598) {
+    if (x > calcLimit) {
         const ln = Math.log(x);
         const primeBound = (offset: number) => x / ln * (1 + offset / ln);
         const min = primeBound(0.992);
@@ -36,10 +46,6 @@ export function countPrimesApprox(x: number): IPrimeApprox {
             max: Math.floor(max)
         };
     }
-    let i = 0;
-    if (x >= 2) {
-        i = -1;
-        while (primes[++i] <= x);
-    }
-    return {avg: i, min: i, max: i};
+    const c = x >= 2 ? countsCache[x - 2] : 0;
+    return {avg: c, min: c, max: c};
 }
