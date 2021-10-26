b4043a4679a0da9e86bc59315261e7b29671c5f3
Vitaly Tomilov <vitaly-t@users.noreply.github.com>
2021-10-26 21:03:05 +0100

    refactoring cache

--- src/cache-primes.ts
+++ src/cache-primes.ts
@@ -1,11 +1,14 @@
 import {sieveIntBoost} from './soe-generators';
 
 /**
- * Maximum cache size that can be specified. It is equal to the
+ * Maximum cache size that can be specified. It equals to the
  * maximum number of primes with gap <= 255, to fit into a byte.
  */
 export const maxCacheSize = 23_163_298;
 
+/**
+ * Primes cache array, returned from cacheArray function.
+ */
 export interface IPrimesArray extends Iterable<number> {
     readonly [index: number]: number;
 
@@ -15,9 +18,12 @@ export interface IPrimesArray extends Iterable<number> {
 /**
  * Creates a compressed cache of prime gaps, so primes can be quickly calculated
  * and accessed, while consuming only 1/8th of memory, compared to number-s.
+ *
+ * Access to primes is very fast, especially with for-of iteration. For index-based
+ * access it uses an optimized list of segments, for faster value calculation.
  */
 export function cachePrimes(n: number): IPrimesArray {
-    const step = Math.floor(7 * Math.log(n)); // page/segment size
+    const step = Math.floor(7 * Math.log(n)); // optimum segment size
     const length = Math.min(n, maxCacheSize);
     const segmentLength = Math.floor(length / step);
     const gaps = new Uint8Array(length - segmentLength);
@@ -37,25 +43,25 @@ export function cachePrimes(n: number): IPrimesArray {
     const obj = {
         length,
         [Symbol.iterator](): Iterator<number> {
-            let a = 0, i = 0, g = 0, k = 0, s = 1;
+            let i = 0, g = 0, k = 0, s = 1, value = 0;
             return {
                 next(): IteratorResult<number> {
                     if (i++ === length) {
                         return {value: undefined, done: true};
                     }
                     if (s++ === step) {
-                        a = segments[k++];
+                        value = segments[k++];
                         s = 1;
                     } else {
-                        a += gaps[g++];
+                        value += gaps[g++];
                     }
-                    return {value: a, done: false};
+                    return {value};
                 }
             };
         }
     };
     return new Proxy<IPrimesArray>(obj, {
-        get: (target: IPrimesArray, prop: any) => {
+        get: (target: any, prop: string | symbol) => {
             const idx = typeof prop === 'string' ? Number(prop) : -1;
             if (idx >= 0) {
                 let a = 0, start = 0, end = idx + 1;
