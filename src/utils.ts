9abb535da88786b2bc24b86bb3a1961e1cf5e4e8
Vitaly Tomilov <vitaly-t@users.noreply.github.com>
2021-10-29 08:41:43 +0100

    refactoring stop-iterators

--- src/utils.ts
+++ src/utils.ts
@@ -1,28 +1,33 @@
 /**
  * Stops an iterator when the callback returns a truthy value.
  */
-export function* stopWhen<T>(iterator: IterableIterator<T>, cb: (value: T, index: number) => boolean): IterableIterator<T> {
-    let i, index = 0;
-    for (; ;) {
-        i = iterator.next();
-        if (i.done || cb(i.value, index++)) {
-            break;
+export function stopWhen<T>(iterator: Iterator<T>, cb: (value: T, index: number) => boolean): Iterable<T> {
+    return {
+        [Symbol.iterator](): Iterator<T> {
+            let index = 0;
+            return {
+                next(): IteratorResult<T> {
+                    const a = iterator.next();
+                    if (a.done || cb(a.value, index++)) {
+                        return {value: undefined, done: true};
+                    }
+                    return a;
+                }
+            };
         }
-        yield i.value;
-    }
-    return i.value;
+    };
 }
 
 /**
  * Stops an iterator, once the count is reached.
  */
-export function stopOnCount<T>(iterator: IterableIterator<T>, count: number): IterableIterator<T> {
+export function stopOnCount<T>(iterator: Iterator<T>, count: number): Iterable<T> {
     return stopWhen(iterator, (value: T, index: number) => index === count);
 }
 
 /**
  * Stops an iterator, upon exceeding a maximum value.
  */
-export function stopOnValue<T>(iterator: IterableIterator<T>, maxValue: T): IterableIterator<T> {
+export function stopOnValue<T>(iterator: Iterator<T>, maxValue: T): Iterable<T> {
     return stopWhen(iterator, (value: T) => value > maxValue);
 }
