049ee95cb46b4329c8f6efa24d0db8e85ce0c3ae
Vitaly Tomilov <vitaly-t@users.noreply.github.com>
2021-11-02 10:42:42 +0000

    adding primeFactors tests

--- test/prime-factors.spec.ts
+++ test/prime-factors.spec.ts
@@ -3,22 +3,21 @@ import {primes} from './primes';
 import {primeFactors} from '../src';
 
 describe('primeFactors', () => {
-    it('must be fast for any small range', () => {
-        expect(primeFactors(2)).to.eql([2]);
-        expect(primeFactors(4)).to.eql([2, 2]);
-        expect(primeFactors(6)).to.eql([2, 3]);
-    }).timeout(1);
+    it('must cover complete initial range', () => {
+        for (let i = 2; i < 1000; i++) {
+            const res = primeFactors(i).reduce((c, i) => c *= i, 1);
+            expect(res).to.eql(i);
+        }
+    });
     it('must work for all prime numbers', () => {
         for (const p of primes) {
             expect(primeFactors(p)).to.eql([p]);
         }
     }).timeout(100); // 100 is minimum for the current implementation :(
-
     it('must handle some interesting cases', () => {
         // from here: https://projecteuler.net/problem=3
         expect(primeFactors(600_851_475_143)).to.eql([71, 839, 1471, 6857]);
     });
-
     it('must handle large complex numbers', () => {
         // TODO: This doesn't work currently (too slow)
         // expect(primeFactors(2 ** 50 - 2)).to.eql([?]);
